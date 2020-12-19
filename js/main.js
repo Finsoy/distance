62bfc4dde5fddda3b5b3bc53963406f93497a349
Finsoy <61739598+Finsoy@users.noreply.github.com>
2020-12-19 16:30:42 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu-sidebar')
+menuButton.addEventListener('click', function () {
+    menu.classList.toggle('active')
+})
